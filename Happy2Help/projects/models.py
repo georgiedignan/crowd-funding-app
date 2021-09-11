7bc99e04d28bd4dcd7bce2ed166b5860c9b3db0a
Georgie Dignan <georgiedignan@gmail.com>
2021-09-11 12:59:45 +1000

    Added conditional in models.py
    Added not_for_profit boolean field and conditional amount field in serializers.py

--- Happy2Help/projects/models.py
+++ Happy2Help/projects/models.py
@@ -3,9 +3,11 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    goal = models.IntegerField()
+    not_for_profit = models.BooleanField(default=False)
+    amount = models.IntegerField(null=True)
     image = models.URLField()
     is_open = models.BooleanField()
+    # can create default
     date_created = models.DateTimeField()
     owner = models.CharField(max_length=200)
 
--- Happy2Help/projects/serializers.py
+++ Happy2Help/projects/serializers.py
@@ -16,11 +16,27 @@ class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
     description = serializers.CharField(max_length=None)
-    goal = serializers.IntegerField()
+    not_for_profit = serializers.BooleanField(default=False)
+    amount = serializers.IntegerField(required=False)
     image = serializers.URLField()
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
     owner = serializers.CharField(max_length=200)
+
+    def validate(self,data):
+        # if data["not_for_profit"] == True:
+            # if 'amount' in data:
+                # raise serializers.ValidationError("")
+        # else:
+            # if 'amount' not in data:
+                # raise serializers.ValidationError("")
+        # return data
+
+        if data["not_for_profit"] == True and 'amount' in data:
+            raise serializers.ValidationError("")
+        elif data["not_for_profit"] == False and 'amount' not in data:
+            raise serializers.ValidationError("")
+        return data
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
